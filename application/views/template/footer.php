515ec12d59687ad3da0a457b744bf477a5aa5f8a
Jorge Armando <jarmandopacheco@gmail.com>
2017-12-18 16:14:51 -0500

    Se cargan los archivos fuentes para este template para proyectos de CodeIgniter y Bootstrap

---
+++ application/views/template/footer.php
@@ -0,0 +1,11 @@
+    <!-- FOOTER -->
+    <footer id="footer">
+        
+    </footer>
+    <!-- jQuery, Bootstrap, Libreries JS, Main JS (Do not remove) and Waypoints-->
+    <script src="<?php echo base_url('vendor/components/jquery/jquery.min.js'); ?>" type="text/javascript"></script>
+    <script src="<?php echo base_url('assets/js/popper.min.js'); ?>" type="text/javascript"></script>
+    <script src="<?php echo base_url('vendor/twbs/bootstrap/dist/js/bootstrap.min.js'); ?>" type="text/javascript"></script>
+    <script src="<?php echo base_url('assets/js/main.js'); ?>" type="text/javascript"></script>
+    </body>
+</html>
---
+++ application/views/template/template_base.php
@@ -0,0 +1,5 @@
+<?php
+	$this->load->view('template/header');
+	$this->load->view($contenido);
+	$this->load->view('template/footer');
+?>
