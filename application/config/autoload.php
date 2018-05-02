f9cfff579d8e7e74c5a02c7b2ce0a04b48a6fc4d
Jorge Armando <jarmandopacheco@gmail.com>
2018-05-02 15:27:01 -0500

    Cargamos la librería Parser como complemento al sistema de layout

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -58,7 +58,7 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array('layout');
+$autoload['libraries'] = array('layout', 'parser');
 
 /*
 | -------------------------------------------------------------------
