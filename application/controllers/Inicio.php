aa7f0e843544a69dedf469eb28a8aafa1155901a
Jorge Armando <jarmandopacheco@gmail.com>
2018-05-02 16:46:46 -0500

    Agregados comentarios descriptivos en el Controller Inicio

--- application/controllers/Inicio.php
+++ application/controllers/Inicio.php
@@ -6,11 +6,17 @@ class Inicio extends CI_Controller {
 	public function __construct()
     {
         parent::__construct();
+        /**
+         * Instaciar siempre la librería para que el sistema de layout fucione correctamente.
+         */
         $this->layout->setLayout('template');
     }
 	
 	public function index()
-	{
+	{	
+		/**
+		 * Estructura para instanciar la vista requerida y que sea renderizada.
+		 */
 		$data['content_for_layout'] = 'inicio';
         $this->parser->parse('layouts/template',$data);
 	}
